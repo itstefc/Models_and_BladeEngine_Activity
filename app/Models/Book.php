0b874ce4e07228d713f9a9511021056f6de9e26b
Stef <67269037+Stef-C@users.noreply.github.com>
2024-07-25 19:15:49 +0800

    Add: Book model with HasFactory

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'isbn',
+        'title',
+        'author',
+        'description',
+        'date_published'
+    ];
 }
